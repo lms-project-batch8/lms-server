f2a4d8dfe28f71384274ca5166f94df2aa96aa6b
souvik-ghosal-007 <souvik13.12.2000@gmail.com>
2024-05-05 15:54:14 +0530

    cors

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
+
 app.get("/search", (req, res) => {
   const { email } = req.query;
 
